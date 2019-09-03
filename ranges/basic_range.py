96c12180d6412e8127d3abca83cd57032bacb7c8
jimmy_shen <jim.morris.shen@gmail.com>
2019-09-03 12:22:09 -0400

    add range

--- ranges/basic_range.py
+++ ranges/basic_range.py
@@ -3,5 +3,15 @@ study range in python
 jimmy shen
 sep 3, 2019
 """
-for i in range(0, 10, 1):
+print('*****************')
+for i in range(10):
+    print(i)
+print('*****************')
+for i in range(10, 0, -1):
+    print(i)
+print('*****************')
+for i in range(9, -1, -1):
+    print(i)
+print('*****************')
+for i in range(9, -1, -2):
     print(i)
