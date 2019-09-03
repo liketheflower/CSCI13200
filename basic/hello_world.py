e42912cbe29d396db0483bf5ef3ed1ac6aa7b3fe
jimmy_shen <jim.morris.shen@gmail.com>
2019-09-03 12:28:04 -0400

    add basic stuff

--- basic/hello_world.py
+++ basic/hello_world.py
@@ -1,1 +1,13 @@
+"""
+This code is printing hello world
+jimmy shen
+Sep 3, 2019
+"""
 print('hello world')
+
+for i in range(10):
+    print('hello world0')
+print('hello world1')
+#while True:
+#    print('hello world0')
+
