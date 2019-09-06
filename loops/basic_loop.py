9d1db077720a8247f2f33fad2cd58d4e9a630db6
jimmy_shen <jim.morris.shen@gmail.com>
2019-09-06 12:02:19 -0400

    update loops

---
+++ loops/basic_loop.py
@@ -0,0 +1,7 @@
+"""
+Basic loops
+jimmy shen
+Sep 6, 2019
+"""
+for i in range(100):
+    print('hello world', i)
---
+++ loops/basic_loop1.py
@@ -0,0 +1,9 @@
+"""
+Basic loops
+jimmy shen
+Sep 6, 2019
+"""
+i = 0
+while i<10:
+    i = i+1
+    print('hello world!', i)
