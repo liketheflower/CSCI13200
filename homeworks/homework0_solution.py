bd15b3e231e55a5fee8dc0d1ddab5d1e17b2b9b6
jimmy_shen <jim.morris.shen@gmail.com>
2019-09-06 11:53:18 -0400

    update all solutions of homework0

--- homeworks/homework0_solution.py
+++ homeworks/homework0_solution.py
@@ -17,10 +17,21 @@ print('a%b', a%b)
 """
 2. what is the data type of varibale c. Where
     c = "I am not a string"
-
+"""
+c = "I am not a string"
+print('solution of question 2', type(c))
+"""
 3. Print out all numbers from 0 to 10(inclusive).
-
+"""
+for i in range(0, 11, 1):
+    print(i)
+"""
 4. Print out all even number from 0 to 10(inclusive).
-
+"""
+for i in range(0, 11, 2):
+    print(i)
+"""
 5. Print out all even number from 0 to 1000(inclusive).
 """
+for i in range(0, 1001, 2):
+    print(i)
