b8aeff22d24260814ed34f635be96cebf36b0520
jimmy_shen <jim.morris.shen@gmail.com>
2019-09-13 11:53:27 -0400

    update solution of homework2

--- homeworks/homework2_solution.py
+++ homeworks/homework2_solution.py
@@ -23,7 +23,7 @@ def print_start_from_1_to_n(n):
     for i in range(1, n+1):
         print('*'*i)
 print_start_from_1_to_n(10)
-print_start_from_1_to_n(2)
+print_start_from_1_to_n(1)
 """
 2. Print 10 to 1 '*' and get output like this:
 **********
@@ -32,10 +32,43 @@ print_start_from_1_to_n(2)
 *
 
 """
+def print_start_from_n_to_1(n):
+    for i in range(n, 0, -1):
+        print('*'*i)
+print_start_from_n_to_1(10)
 """
 3. write a function to output 1 to n '*'.
 
 4. write a function to output 1 to n '*' and then n-1 '*' to 1 '*'.
+"""
+def print_start_from_1_to_n_back_to_1(n):
+    print_start_from_1_to_n(n)
+    print_start_from_n_to_1(n-1)
+    """
+    for i in range(1, n+1):
+        print('*'*i)
+    for i in range(n-1, 0, -1):
+        print('*'*i)
+    """
+print_start_from_1_to_n_back_to_1(9)
 
+"""
 5. write a function to get the sum of 1 to n(inclusively)
 """
+def get_sum_from_1_to_n(n):
+    sums = 0
+    for i in range(1, n+1):
+        print('i:', i,'old sums:', sums)
+        sums = sums + i
+        print('i:', i,'new_sums:', sums)
+    return sums
+n = 100
+sums = get_sum_from_1_to_n(n)
+print('sum from 1 to',n ,' is:', sums)
+print('another way to solve this problem')
+
+sums = sum(list(range(1,n+1)))
+print('sum from 1 to',n ,' is:', sums)
+
+    
+
