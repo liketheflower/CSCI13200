2b47463c0af23d507f00eabd2c8bf050e9945599
jimmy_shen <jim.morris.shen@gmail.com>
2019-10-22 11:37:51 -0400

    update

---
+++ ascii/advanced_accii.py
@@ -0,0 +1,7 @@
+for c in 'abcdefghijklmnopqrstuvwxyz':
+    print(c)
+
+print([chr(97+i) for i in range(26)])
+print([chr(ord('a')+i) for i in range(26)])
+print([chr(ord('A')+i) for i in range(26)])
+    
