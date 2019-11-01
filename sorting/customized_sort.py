4e0d728a4dc5182c87fd2d7a9b0040960e003ca4
jimmy_shen <jim.morris.shen@gmail.com>
2019-11-01 12:03:44 -0400

    update

--- sorting/customized_sort.py
+++ sorting/customized_sort.py
@@ -3,3 +3,11 @@ def my_add(a, b):
 def my_add_new(func, a,b):
     return func(a, b)
 print(my_add_new(my_add, 2,3))
+
+a = [1, -2, 9, -10]
+def identity(x):return x
+def my_reverse(x):return -x
+print(sorted(a))
+print(sorted(a, key=my_reverse))
+print(sorted(a, key=lambda x:-x))
+
