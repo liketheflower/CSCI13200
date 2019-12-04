b9aae1269dc0229f85b476578cce521fa4656119
jimmy_shen <jim.morris.shen@gmail.com>
2019-12-04 11:19:23 -0500

    update partc_1 code

--- homeworks/exam_samples/partc_1.py
+++ homeworks/exam_samples/partc_1.py
@@ -2,7 +2,7 @@
 import collections
 def favorite_car_color(car_colors):
     cnt = collections.Counter(car_colors)
-    color_cnt = (cnt.items())
+    color_cnt = list(cnt.items())
     max_color = max(color_cnt, key = lambda x:x[1])
     return max_color[0]
 car_colors = ['white', 'grey', 'white', 'grey', 'black', 'silver', 'green', 'black', 'white']
